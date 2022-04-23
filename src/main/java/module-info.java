0ce0ac00618df8c1b9bb27d6846aaef3481adf8f
Bruno Salmon <dev.salmonb@gmail.com>
2022-04-23 21:13:20 +0100

    Extracted code from Mandelbrot demo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.lib.tracerframework {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires webfx.lib.odometer;
+    requires webfx.platform.client.uischeduler;
+    requires webfx.platform.shared.json;
+    requires webfx.platform.shared.log;
+    requires webfx.platform.shared.webworker;
+
+    // Exported packages
+    exports dev.webfx.lib.tracerframework;
+
+}
